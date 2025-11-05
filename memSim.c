e2f63cb14167f9993a9c085dd0fbbe4d4bb72895
Dwight Anthony Taylor Jr <dtaylo37@unix5.csc.calpoly.edu>
2025-11-05 14:04:31 -0800

    memSim.c changes to functions

--- memSim.c
+++ memSim.c
@@ -3,51 +3,80 @@
 #include <string.h>
 #include "memSim.h"
 
-TLBEntry tlb[TLB_SIZE];
-PageTableEntry page_table[PAGE_TABLE_SIZE];
-char physical_mem[256][FRAME_SIZE];
-Frame frame_info[256];
-int num_frames;
-char *pra_algorithm;
 
 // statistic tracking
-int tlb_hits = 0;
-int tlb_misses = 0;
-int page_faults = 0;
 int total_addresses = 0;
 
 // counters
-int tlb_fifo_index = 0;
-int next_frame = 0;
 int time_counter = 0;
 
-// initialize all structs in header file
-void initialize_tlb() {}
+// all function definitions
 
-void initializePT() {}
+//initialize tlb
+void init_tlb(TLB *tlb) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+	tlb->entries[i].page_num = -1;
+	tlb->entries[i].frame_num = -1;
+	tlb->entries[i].valid = false;
+    }
+    
+    tlb->fifo_index = 0;
+    tlb->hits = 0;
+    tlb->misses = 0;
+}
 
-int search_tlb(int page_num) {}
+// lookup TLB page
+int tlb_lookup(TLB *tlb, int page_num) {}
 
-void update_tlb(int page_num, int frame_num) {}
+// insert into TLB
+void tlb_insert(TLB *tlb, int page_num, int frame_num) {}
 
-int fifo_replacement() {}
+// invalidates entry in TLB
+void tlb_invalidate(TLB *tlb, int page_num) {}
 
-int lru_replacement() {}
+// initializes page table
+void init_page_table(PageTable *pt) {
+    for (int i = 0; i < PAGE_TABLE_SIZE; i++) {
+	pt->entries[i].frame_num = -1;
+	pt->entries[i].loaded = false;
+    }
+}
 
-int opt_replacement(int *future_refs, int future_count) {}
+// looks up page in page table
+int page_table_lookup(PageTable *pt, int page_num) {}
 
-int handle_page_fault(intpage_num, FILE *backing_store, int *future_refs, int future_count) {}
+// delete page table from page
+void page_table_evict(PageTable *pt, int page_num) {}
 
-void translate_address(int logical_address, FILE *backing_store, int *future_refs, int future_count) {}
+// initialize physical memory
+void init_physical_memory(PhysicalMemory *pm, int num_frames) {}
 
-void print_stats() {
-  double page_fault_rate = (double)page_faults / total_addresses * 100.0;
-  double tlb_miss_rate = (double)tlb_misses / total_addresses * 100.0;
+// check if free frames, if not return false
+bool has_free_frame(PhysicalMemory *pm) {}
+
+// get next free frame
+int get_free_frame(PhysicalMemory *pm) {}
+
+// loads page into a specific frame
+void load_page_to_frame(PhysicalMemory *pm, int frame_num, int page_num, unsigned char *page_data) {}
+
+// initializes fifo queue
+void init_fifo_queue(FIFOQueue *queue, int capacity) {}
+
+// adds frame into queue
+void fifo_enqueue(FIFOQueue *queue, int frame_num) {}
+
+// removes frame from queue
+int fifo_dequeue(FIFOQueue *queue) {}
+
+void print_stats(MemorySimulator *sim) {
+  double page_fault_rate = (double)sim->page_faults / total_addresses * 100.0;
+  double tlb_miss_rate = (double)sim->tlb.misses / total_addresses * 100.0;
 
   printf("Number of Translated Addresses = %d\n", total_addresses);
-  printf("Page Faults = %d\n", page_faults);
+  printf("Page Faults = %d\n", sim->page_faults);
   printf("Page Fault Rate = %.3f\n", page_fault_rate);
-  printf("TLB Hits = %d\n", tlb_hits);
-  printf("TLB Misses = %d\n", tlb_misses);
+  printf("TLB Hits = %d\n", sim->tlb.hits);
+  printf("TLB Misses = %d\n", sim->tlb.misses);
   printf("TLB Hit Rate = %.3f\n", 100.0 - tlb_miss_rate);
 }
